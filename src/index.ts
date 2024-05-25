b82fe4dda7993c3181efb7d42b887ccdba951d69
多bug的啸天犬 <morebugofdog@outlook.com>
2024-05-25 19:46:06 +0800

    设置初始模版

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,24 @@
 
 ;(function (Scratch) {
-  const SkydogCookieIconUrl ='https://m.ccw.site/user_projects_assets/37848819fcc08004615ec96170b3635c.png'
-  const SkydogCookiePictureUrl ='https://m.ccw.site/user_projects_assets/8c2404f1490aad6019616fda9e609133.jpg'
+  const ExtIconUrl =''
+  const ExtPictureUrl =''
   if (Scratch.extensions.unsandboxed === false) {
     throw new Error('Sandboxed mode is not supported')
   }
   // Your extension's code
   Scratch.translate.setup({
     'zh-cn': {
-      'SkydogCookie.name': '啸天犬的Cookie',
-      'SkydogCookie.newCookie':
-        '创建或修改名为[name]的cookie值为[value]过期时间[time]天',
-      'SkydogCookie.read': '读取名为[name]的cookie值',
-      'SkydogCookie.delete': '删除名为[name]的cookie'
+      'Ext.name': '啸天犬的Cookie',
     },
     en: {
-      'SkydogCookie.name': "Skydog's Cookie",
-      'SkydogCookie.newCookie':
-        'Create or modify a cookie named [name] with value [value] and expiration time [time]',
-      'SkydogCookie.read': 'Read the value of the cookie named [name]',
-      'SkydogCookie.delete': 'Delete the cookie named [name]'
+      'Ext.name': "Skydog's Cookie",
     }
   })
   function i10n(id: string) {
     return Scratch.translate({ id, default: id, description: id })
   }
 
-  class SkydogCookie implements Scratch.Extension {
+  class Ext implements Scratch.Extension {
     runtime: VM.Runtime
     constructor(runtime: VM.Runtime) {
       this.runtime = runtime
@@ -34,17 +26,17 @@
 
     getInfo() {
       return {
-        id: 'SkydogCookie',
-        name: i10n('SkydogCookie.name'),
+        id: 'Ext',
+        name: i10n('Ext.name'),
         color1: '#996026',
         color2: '#996026',
-        blockIconURI:SkydogCookieIconUrl,
-        menuIconURI:SkydogCookieIconUrl,
+        blockIconURI:ExtIconUrl,
+        menuIconURI:ExtIconUrl,
         blocks: [
           {
             blockType: Scratch.BlockType.COMMAND,
             opcode: 'newCookie',
-            text: i10n('SkydogCookie.newCookie'),
+            text: i10n('Ext.newCookie'),
             arguments: {
               name: {
                 type: Scratch.ArgumentType.STRING,
@@ -62,81 +54,35 @@
               }
             }
           },
-          {
-            blockType: Scratch.BlockType.REPORTER,
-            opcode: 'read',
-            text: i10n('SkydogCookie.read'),
-            arguments: {
-              name: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'hello'
-              }
-            }
-          },
-          {
-            blockType: Scratch.BlockType.COMMAND,
-            opcode: 'delete',
-            text: i10n('SkydogCookie.delete'),
-            arguments: {
-              name: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'hello'
-              }
-            }
-          }
+          
         ]
       }
     }
-    newCookie(args) {
-      const name = args.name
-      const value = args.value
-      const time = args.time
-      const host = window.location.href
-      // 加入前缀，防止读取非作品信息
-      const cookie = `ccw_custom_cookie_${host}_${name}=${value};expires=${new Date(Date.now() + time * 24 * 60 * 60 * 1000).toUTCString()};path=/`
-      document.cookie = cookie
-    }
-    read(args) {
-      const name = args.name
-      const cookies = document.cookie.split(';')
-      let value = ''
-      cookies.forEach(cookie => {
-        const [key, val] = cookie.split('=')
-        if (
-          key.trim() === `ccw_custom_cookie_${window.location.href}_${name}`
-        ) {
-          value = val.trim()
-        }
-      })
-      return value
-    }
-    delete(args) {
-      const name = args.name
-      document.cookie = `ccw_custom_cookie_${window.location.href}_${name}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`
-    }
+    //extension's code here
+
+    
   }
-  // The following snippet ensures compatibility with Turbowarp / Gandi IDE. If you want to write Turbowarp-only or Gandi-IDE code, please remove corresponding code
     // For Gandi
     window.tempExt = {
-      Extension: SkydogCookie,
+      Extension: Ext,
       info: {
-          name: "SkydogCookie.name",
-          description: "SkydogCookie.descp",
-          extensionId: "SkydogCookie",
-          iconURL: SkydogCookiePictureUrl,
-          insetIconURL: SkydogCookieIconUrl,
+          name: "Ext.name",
+          description: "Ext.descp",
+          extensionId: "Ext",
+          iconURL: ExtPictureUrl,
+          insetIconURL: ExtIconUrl,
           featured: true,
           disabled: false,
           collaborator: "多bug的啸天犬 @ CCW"
       },
       l10n: {
           "zh-cn": {
-              "SkydogCookie.name": "啸天犬的Cookie",
-              "SkydogCookie.descp": "操纵Cookie！设置过期时间！"
+              "Ext.name": "啸天犬的Cookie",
+              "Ext.descp": "操纵Cookie！设置过期时间！"
           },
           en: {
-              "SkydogCookie.name": "Skydog's Cookie",
-              "SkydogCookie.descp": "Manipulate cookies! Set expiration time!"
+              "Ext.name": "Skydog's Cookie",
+              "Ext.descp": "Manipulate cookies! Set expiration time!"
           }
       }
   };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,24 +1,22 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  name: 'SkydogCookie', // Replace it with your extension name
+  name: 'Ext', // Replace it with your extension name
   entry: ['src/index.ts', 'src/index.js'],
   target: ['esnext'],
   format: ['iife'],
   outDir: 'dist',
   banner: {
     // Replace it with your extension's metadata
-    js: `// Name: Skydog's Cookie
+    js: `// Name: Ext
 // Version: 1.0.0
-// ID: SkydogCookie
+// ID: Ext
 // Description: Manipulate cookies! Set expiration time!
 // By: MoreBugOfDog
 // License: MPL-2.0
 // 本拓展经过开发者严格的法律审查，不涉及任何违法行为。
 // 使用本拓展造成的任何后果与开发者无关。
-// Cookie的读取和储存强制添加了唯一标识符和命名空间（ccw_custom_作品链接_用户输入的name），
-// path强制为/ 防止随请求携带导致错误
-// 编辑器内和编辑器外cookie不同步，这是为了方便肝酱开发，非Bug
+
 `
   },
   platform: 'browser',
