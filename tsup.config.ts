6db46d8891a1ce566d96a0dfa8cf17876b2d6948
多bug的啸天犬 <morebugofdog@outlook.com>
2024-06-13 21:11:07 +0800

    update

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   outDir: 'dist',
   banner: {
     // Replace it with your extension's metadata
-    js: `// Name: Ext
+    js: `// Name: Better Message
 // Version: 1.0.0
-// ID: Ext
-// Description: Manipulate cookies! Set expiration time!
-// By: MoreBugOfDog
-// License: MPL-2.0
+// ID: BetterMsg
+// Description: 更好的弹窗！美观|实用|丰富
+// By: Skydog
+// License: MIT
 // 本拓展经过开发者严格的法律审查，不涉及任何违法行为。
 // 使用本拓展造成的任何后果与开发者无关。
 
