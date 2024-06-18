7f48509359ba6271180779b9b454e434d399263b
多bug的啸天犬 <morebugofdog@outlook.com>
2024-06-18 21:58:54 +0800

    fix(start loacl app):修复通过https://xxx.xxx/?可绕过安全性参数校验的问题

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,14 +8,16 @@ export default defineConfig({
   outDir: 'dist',
   banner: {
     // Replace it with your extension's metadata
-    js: `// Name: Ext
+    js: `// Name: Local App
 // Version: 1.0.0
-// ID: Ext
-// Description: Manipulate cookies! Set expiration time!
+// ID: LocalApp
+// Description: Link local app directly! CCW Launcher preset!
 // By: MoreBugOfDog
 // License: MPL-2.0
 // 本拓展经过开发者严格的法律审查，不涉及任何违法行为。
 // 使用本拓展造成的任何后果与开发者无关。
+// This extension has been reviewed by a professional lawyer and does not contain any illegal activities.
+// Any consequences resulting from the use of this extension are solely the responsibility of the developer.
 
 `
   },
