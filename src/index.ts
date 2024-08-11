380c82e7118cb19353bec729b75a9395474ac592
多bug的啸天犬 <morebugofdog@outlook.com>
2024-08-11 20:57:28 +0800

    feat(has-bugs):内联工程

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
 
 ;(function (Scratch) {
-  const ExtIconUrl =''
-  const ExtPictureUrl =''
+  const EDPExtIconUrl =''
+  const EDPExtPictureUrl =''
   if (Scratch.extensions.unsandboxed === false) {
     throw new Error('Sandboxed mode is not supported')
   }
   // Your extension's code
+  // 已确认I10n无误
   Scratch.translate.setup({
     'zh-cn': {
-      'Ext.name': '啸天犬的Cookie',
+      'EmbeddedProject.name': '作品内联',
+      'EmbeddedProject.edp':'嵌入作品id为[id]的作品(覆盖舞台区) |⚠️| 作品id是作品详情页网址"detail/"后"?"前的数字与字母的组合',
+      'EmbeddedProject.deledp':'删除作品id为[id]的内联作品'
     },
     en: {
-      'Ext.name': "Skydog's Cookie",
+      'EmbeddedProject.name': "Embedded Project",
+      'EmbeddedProject.edp':'EmbeddedProject with work id [id] (covering the stage area) |⚠️| The work id is after "detail/"?" Before the combination of numbers and letters',
+      'EmbeddedProject.deledp':'Delete inline work with work id [id]'
     }
   })
   function i10n(id: string) {
     return Scratch.translate({ id, default: id, description: id })
   }
 
-  class Ext implements Scratch.Extension {
+  class EmbeddedProject implements Scratch.Extension {
     runtime: VM.Runtime
     constructor(runtime: VM.Runtime) {
       this.runtime = runtime
@@ -26,31 +31,32 @@
 
     getInfo() {
       return {
-        id: 'Ext',
-        name: i10n('Ext.name'),
-        color1: '#996026',
-        color2: '#996026',
-        blockIconURI:ExtIconUrl,
-        menuIconURI:ExtIconUrl,
+        id: 'EmbeddedProject',
+        name: i10n('EmbeddedProject.name'),
+        color1: '#83c379',
+        color2: '#83c379',
+        blockIconURI:EDPExtIconUrl,
+        menuIconURI:EDPExtIconUrl,
         blocks: [
           {
             blockType: Scratch.BlockType.COMMAND,
-            opcode: 'newCookie',
-            text: i10n('Ext.newCookie'),
-            arguments: {
-              name: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'hello'
-              },
-
-              value: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'word'
-              },
-
-              time: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 30
+            opcode: 'edp',
+            text: i10n('EmbeddedProject.edp'),
+            arguments:{
+              id:{
+                type:'string',
+                defaultValue:'66b2345e4f2841189434393b'
+              }
+            }
+          },
+          {
+            blockType: Scratch.BlockType.COMMAND,
+            opcode: 'deledp',
+            text: i10n('EmbeddedProject.deledp'),
+            arguments:{
+              id:{
+                type:'string',
+                defaultValue:'66b2345e4f2841189434393b'
               }
             }
           },
@@ -59,30 +65,102 @@
       }
     }
     //extension's code here
+    edp(args){
+      
+      
+      
+      
+      // 异步执行，防止嵌入操作阻塞线程
+      async function edps(id){
+        // 在class属性为gandi_stage_stage_1fD7k的DOM上叠加一层iframe，url为baidu.com
+            
+      const targetElement = document.querySelector('.gandi_stage_stage_1fD7k');
 
-    
+      if (targetElement) {
+          
+          const iframe = document.createElement('iframe');
+          
+          const iframeWindow = iframe.contentWindow;
+          
+          const iframeDocument = iframeWindow.document;
+          
+          iframe.src = 'https://www.ccw.site/player/'+ id;
+          
+          // 设置iframe的样式
+          iframe.style.width = '100%';
+          iframe.style.height = '100%';
+          iframe.style.border = 'none';
+          iframe.style.position = 'absolute';
+          iframe.style.top = '0';
+          iframe.style.left = '0';
+          iframe.id = 'ccw_user_extension_embedded_project_iframe' + id
+          
+          
+          // 将iframe添加到目标DOM元素中
+          targetElement.appendChild(iframe);
+
+          // 监听 iframe 的全屏请求事件
+          iframe.addEventListener('requestFullscreen', (event) => {
+            event.preventDefault();
+            return false;
+          });
+          //等待出现iframe内出现class为main-module_fullscreen-button_9f565的DOM元素，并点击
+          function waitElement(selector, callback) {
+            const element = iframeDocument.querySelector(selector);
+            if (element) {
+              callback();
+            } else {
+              setTimeout(() => {
+                waitElement(selector, callback);
+              }, 100);
+            }
+          }
+          waitElement('.main-module_fullscreen-button_9f565', () => {
+            const fullscreenButton = iframeDocument.querySelector('.main-module_fullscreen-button_9f565');
+            fullscreenButton.click();
+
+          });
+
+          
+      }
+      }
+      const id = args.id;
+      edps(id);
+        }
+      deledp(args){
+        const id = args.id;
+        async function deledps(id){
+        
+        const targetElement = document.querySelector('#ccw_user_extension_embedded_project_iframe'+id);
+        if(targetElement){
+          targetElement.remove();
+        }
+        }
+        deledps(id);
+        
+      }
   }
     // For Gandi
     window.tempExt = {
-      Extension: Ext,
+      Extension: EmbeddedProject,
       info: {
-          name: "Ext.name",
-          description: "Ext.descp",
-          extensionId: "Ext",
-          iconURL: ExtPictureUrl,
-          insetIconURL: ExtIconUrl,
+          name: "EmbeddedProject.name",
+          description: "EmbeddedProject.descp",
+          extensionId: "EmbeddedProject",
+          iconURL: EDPExtPictureUrl,
+          insetIconURL: EDPExtIconUrl,
           featured: true,
           disabled: false,
           collaborator: "多bug的啸天犬 @ CCW"
       },
       l10n: {
           "zh-cn": {
-              "Ext.name": "啸天犬的Cookie",
-              "Ext.descp": "操纵Cookie！设置过期时间！"
+              "EmbeddedProject.name": "啸天犬的内联工程",
+              "EmbeddedProject.descp": "在项目中内嵌其他项目！无缝衔接！无需跳转！"
           },
           en: {
-              "Ext.name": "Skydog's Cookie",
-              "Ext.descp": "Manipulate cookies! Set expiration time!"
+              "EmbeddedProject.name": "Skydog's Embedded Project",
+              "EmbeddedProject.descp": "Embed other items in your project! Seamless! No jump!"
           }
       }
   };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  name: 'Ext', // Replace it with your extension name
+  name: 'EmbeddedProject', // Replace it with your extension name
   entry: ['src/index.ts', 'src/index.js'],
   target: ['esnext'],
   format: ['iife'],
   outDir: 'dist',
   banner: {
     // Replace it with your extension's metadata
-    js: `// Name: Ext
+    js: `// Name: EmbeddedProject
 // Version: 1.0.0
-// ID: Ext
-// Description: Manipulate cookies! Set expiration time!
-// By: MoreBugOfDog
+// ID: EmbeddedProject
+// Description: Embed other items in your project! Seamless! No jump!
+// Description_zh: 在项目中内嵌其他项目！无缝衔接！无需跳转！
+// By: 多bug的啸天犬
 // License: MPL-2.0
 // 本拓展经过开发者严格的法律审查，不涉及任何违法行为。
 // 使用本拓展造成的任何后果与开发者无关。
